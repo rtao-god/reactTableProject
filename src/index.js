67b2f4f8123576450fdba60b7ad32bc82309a170
rtao-god <110540468+rtao-god@users.noreply.github.com>
2023-02-06 21:19:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import store from './components/state'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+let renderTree = (state) => {
+  root.render(
+    <HashRouter>
+      <App state={state} dispatch={store.dispatch.bind(store)} />
+    </HashRouter>
+  )
+}
+
+renderTree(store.getState())
+
+store.subscride(renderTree)
