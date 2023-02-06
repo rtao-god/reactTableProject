1595a1cb9af9a94a7ec36a0d1d5c58a87f36210a
rtao-god <110540468+rtao-god@users.noreply.github.com>
2023-02-06 21:18:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import ProductTable from './components/productTable/ProductTable'
+import ProductView from './components/productView/ProductView'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+function App(props) {
   return (
+      <Routes>
+        <Route path="/" element={<ProductTable productTable={props.state.productTable} dispatch={props.dispatch} />} />
+        <Route path="/productView" element={<ProductView />} />
+      </Routes>
   )
 }
 
-export default App;
+export default App
